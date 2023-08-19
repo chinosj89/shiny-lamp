996822ca10d5ce66c6179aa2542aca6a5d09cc52
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-08-19 16:31:45 -0700

    svg is create

--- index.js
+++ index.js
@@ -3,50 +3,79 @@ const fs = require('fs');
 const userInfo = require('./package.json')
 const generateLogo = require('./logo')
 const questions = [
-inquirer
-    .prompt([
-        {
-            type: 'input',
-            message: 'Initials? Please write up to 3 characters only',
-            name: 'initials',
-            maxLength: 3
-        },
-        {
-            type: 'input',
-            message: 'What color would you like for the text? Provide either the name of the color or the hexadecimal number',
-            name: 'text-color',
-        },
-        {
-            type: 'list',
-            message: 'What shape would you like your logo to be?',
-            choices: [
-                'circle',
-                'triangle',
-                'square'],
-            name: 'shapes',
-        },
-        {
-            type: 'input',
-            message: 'What color would you like for the background? Provide either the name of the color or the hexadecimal number',
-            name: 'background-color',
-        },
-    
-    ])
+
+    {
+        type: 'input',
+        message: 'Initials? Please enter up to 3 characters only',
+        name: 'initials',
+        validate: (input) => {
+            if (input.length > 3 || input.length === 0) {
+                return new Error('Please enter up to 3 characters only.');
+            }
+            return true;
+        }
+    },
+    {
+        type: 'list',
+        message: 'What color would you like for the text?',
+        name: 'textColor',
+        choices: [
+            'Crimson',
+            'ForestGreen',
+            'Gold',
+            'MediumPurple',
+            'SlateBlue',
+            'DarkOrange',
+            'White',
+            'Black'
+        ],
+    },
+    {
+        type: 'list',
+        message: 'What color would you like for the background?',
+        name: 'backgroundColor',
+        choices: [
+            'Crimson',
+            'ForestGreen',
+            'Gold',
+            'MediumPurple',
+            'SlateBlue',
+            'DarkOrange',
+            'White',
+            'Black'
+        ],
+        validate: (questions, list) => {
+            if (questions['text-color'].length === list) {
+                return new Error('Text color and Background are the same color, please choose something else');
+            }
+            return true;
+        }
+    },
+    {
+        type: 'list',
+        message: 'What shape would you like your logo to be?',
+        choices: [
+            'circle',
+            'triangle',
+            'square'],
+        name: 'shapes',
+    },
 ];
 
-    function writeToFile(fileName, data) {
-        fs.writeFile(fileName, data, (err) =>
-          err ? console.error('Error!', err) : console.log('See your Logo!'))
-      }
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.error('Error!', err) : console.log('See your Logo!'))
+}
 
-    function init() {
-        const logoEl = [];
-        return inquirer.prompt(questions)
+function init() {
+    const logoEl = [];
+    return inquirer.prompt(questions)
         .then((data) => {
             if (data) {
+                console.log(data);
                 logoEl.push(generateLogo(data));
                 writeToFile('logo.svg', logoEl.join('\n'))
             }
         })
-    }
-    init();
+}
+init();
--- logo.js
+++ logo.js
@@ -2,24 +2,72 @@
 function renderShapes(selectedShape) {
     switch (selectedShape) {
         case 'circle':
-            return '<circle cx="150" cy="100" r="80" fill="green" />';
+            return 'circle cx="150" cy="100" r="80"';
         case 'triangle':
-            return '<polygon points="150,20 280,180 20,180" fill="blue" />';
+            return 'polygon points="150,20 280,180 20,180"';
         case 'square':
-            return '<rect x="50" y="50" width="200" height="200" fill="red" />';
+            return 'rect x="50" y="50" width="200" height="200"';
         default:
             return '';
     }
 }
+function renderBg(selectedColor) {
+    switch (selectedColor) {
+        case 'Crimson':
+          return 'fill="Crimson"';
+        case 'Lavender':
+          return 'fill="Lavender"';
+        case 'ForestGreen':
+          return 'fill="ForestGreen"';
+        case 'Gold':
+          return 'fill="Gold"';
+        case 'MediumPurple':
+          return 'fill="MediumPurple"';
+        case 'SlateBlue':
+          return 'fill="SlateBlue"';
+        case 'DarkOrange':
+          return 'fill="DarkOrange"';
+        case 'White':
+          return 'fill="White"';
+        case 'Black':
+          return 'fill="Black"';
+        default:
+          return '';
+    }
+}
 
+function renderText(selectedColor) {
+    switch (selectedColor) {
+        case 'Crimson':
+          return 'fill="Crimson"';
+        case 'ForestGreen':
+          return 'fill="ForestGreen"';
+        case 'Gold':
+          return 'fill="Gold"';
+        case 'MediumPurple':
+          return 'fill="MediumPurple"';
+        case 'SlateBlue':
+          return 'fill="SlateBlue"';
+        case 'DarkOrange':
+          return 'fill="DarkOrange"';
+        case 'White':
+          return 'fill="White"';
+        case 'Black':
+          return 'fill="Black"';
+        default:
+          return '';
+    }
+}
 function generateLogo(data) {
     const shape = renderShapes(data.shapes);
+    const bg = renderBg(data.backgroundColor)
+    const txtColor = renderText(data.textColor)
     return `
     <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-    <${shape} ${bgColor} />
+    <${shape} ${bg} />
 
-    <text x="150" y="125" font-size="60" text-anchor="middle" fill="white">SVG</text>
+    <text x="150" y="125" font-size="60" text-anchor="middle" ${txtColor}>${data.initials}</text>
 
     </svg>`
 }
