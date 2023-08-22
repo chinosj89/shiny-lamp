5acd1b7da1a6c0b7528c94732244c73b4ae6ec8e
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-08-22 14:00:12 -0700

    removed switch statements to satisfy acceptance criteria

--- logo.js
+++ logo.js
@@ -4,6 +4,7 @@ const Triangle = require('./lib/triangle');
 const Square = require('./lib/square');
 const Shape = require('./lib/shapes')
 
+//acceptance criteria only required different tests and js for the shapes. colors made to switch statements instead
 function renderShapes(selectedShape, data) {
     switch (selectedShape) {
       case 'circle':
@@ -17,32 +18,10 @@ function renderShapes(selectedShape, data) {
     }
   }
   
-// swtich statements for background color
-function renderBg(selectedColor) {
-    switch (selectedColor) {
-        case 'Crimson':
-            return 'fill="Crimson"';
-        case 'Lavender':
-            return 'fill="Lavender"';
-        case 'ForestGreen':
-            return 'fill="ForestGreen"';
-        case 'Gold':
-            return 'fill="Gold"';
-        case 'MediumPurple':
-            return 'fill="MediumPurple"';
-        case 'SlateBlue':
-            return 'fill="SlateBlue"';
-        case 'DarkOrange':
-            return 'fill="DarkOrange"';
-        case 'White':
-            return 'fill="White"';
-        case 'Black':
-            return 'fill="Black"';
-        default:
-            return '';
-    }
-}
+
 // switch statements for text color
+// originally had switch statements for background as well but since the acceptance criteria required that the color is tested within the shapes, i had to remove the switch statements here. the choice by the user is not added within the const shape within the function generateLogo
+// testing is also done for the shapes along with a renderBg 
 function renderText(selectedColor) {
     switch (selectedColor) {
         case 'Crimson':
@@ -68,13 +47,13 @@ function renderText(selectedColor) {
 
 // function to generate svg file
 function generateLogo(data) {
-    const shape = renderShapes(data.shapes, data);
-    const bg = renderBg(data.backgroundColor)
+    const shape = renderShapes(data.shapes, data.backgroundColor);
+    // const bg = renderBg(data.backgroundColor)
     const txtColor = renderText(data.textColor)
     return `
     <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-    <${shape.render()} ${bg} />
+    <${shape.render()} />
 
     <text x="150" y="125" font-size="60" text-anchor="middle" ${txtColor}>${data.initials}</text>
 
---
+++ readMe.md
@@ -0,0 +1,60 @@
+
+  # Module 10: SVG Logo Maker
+
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+ 
+  ## Description
+  This project is about using our knowledge of Node.js and OOP and use application prompts to generate a SVG file
+  <!-- Add a screenshot of your application here -->
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Credits](#credits)
+  - [License](#license)
+  - [Contribution](#how-to-contribute)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  Fork or Clone the repository; or npm install
+  
+  ## Usage 
+  ```
+  GIVEN a command-line application that accepts user input
+WHEN I am prompted for text
+THEN I can enter up to three characters
+WHEN I am prompted for the text color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I am prompted for a shape
+THEN I am presented with a list of shapes to choose from: circle, triangle, and square
+WHEN I am prompted for the shape's color
+THEN I can enter a color keyword (OR a hexadecimal number)
+WHEN I have entered input for all the prompts
+THEN an SVG file is created named `logo.svg`
+AND the output text "Generated logo.svg" is printed in the command line
+WHEN I open the `logo.svg` file in a browser
+THEN I am shown a 300x200 pixel image that matches the criteria I entered
+  ```
+  
+  ## Credits
+  None
+  
+  ## License
+  This project uses the [MIT License](https://opensource.org/licenses/MIT) license
+  
+  ## How to Contribute
+  Email Me!
+  
+  ## Tests
+  Unit Testing for each shapes was done, along with the colors
+
+  ## Deployment
+  Since this website will not be deployed; see link for demo: 
+  
+  ## Questions
+  ### Github: https://github.com/chinosj89
+  ### Email: raphael.sanjuan13@gmail.com
+  
+### ReadMe 
+ReadME created through personal ReadMe Generator from: https://github.com/chinosj89/cuddly-invention
