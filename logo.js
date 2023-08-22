7586c89c11c80ee1a01a6b4995247a91b8da4b6c
raphael.sanjuan13@gmail.com <raphael.sanjuan13@gmail.com>
2023-08-21 17:27:49 -0700

    shapes should be in class not function

--- logo.js
+++ logo.js
@@ -1,75 +1,115 @@
-// learned switch statements will help in this case since i have multiple choices as well
-function renderShapes(selectedShape) {
-    switch (selectedShape) {
-        case 'circle':
-            return 'circle cx="150" cy="100" r="80"';
-        case 'triangle':
-            return 'polygon points="150,20 280,180 20,180"';
-        case 'square':
-            return 'rect x="50" y="50" width="200" height="200"';
-        default:
-            return '';
+class Shape {
+    constructor() {
+        this.type = '';
+    }
+
+    render() {
+        return '';
+    }
+}
+class Circle extends Shape {
+    constructor() {
+        super();
+        this.type = 'circle';
+    }
+
+    render() {
+        return 'circle cx="150" cy="100" r="80"';
     }
 }
 
+class Triangle extends Shape {
+    constructor() {
+        super();
+        this.type = 'triangle';
+    }
+
+    render() {
+        return 'polygon points="150,20 280,180 20,180"';
+    }
+}
+
+class Square extends Shape {
+    constructor() {
+        super();
+        this.type = 'square';
+    }
+
+    render() {
+        return 'rect x="50" y="50" width="200" height="200"';
+    }
+}
+function renderShapes(selectedShape, data) {
+    switch (selectedShape) {
+      case 'circle':
+        return new Circle(data);
+      case 'triangle':
+        return new Triangle(data);
+      case 'square':
+        return new Square(data);
+      default:
+        return new Shape(); 
+    }
+  }
+  
 // swtich statements for background color
 function renderBg(selectedColor) {
     switch (selectedColor) {
         case 'Crimson':
-          return 'fill="Crimson"';
+            return 'fill="Crimson"';
         case 'Lavender':
-          return 'fill="Lavender"';
+            return 'fill="Lavender"';
         case 'ForestGreen':
-          return 'fill="ForestGreen"';
+            return 'fill="ForestGreen"';
         case 'Gold':
-          return 'fill="Gold"';
+            return 'fill="Gold"';
         case 'MediumPurple':
-          return 'fill="MediumPurple"';
+            return 'fill="MediumPurple"';
         case 'SlateBlue':
-          return 'fill="SlateBlue"';
+            return 'fill="SlateBlue"';
         case 'DarkOrange':
-          return 'fill="DarkOrange"';
+            return 'fill="DarkOrange"';
         case 'White':
-          return 'fill="White"';
+            return 'fill="White"';
         case 'Black':
-          return 'fill="Black"';
+            return 'fill="Black"';
         default:
-          return '';
+            return '';
     }
 }
 // switch statements for text color
 function renderText(selectedColor) {
     switch (selectedColor) {
         case 'Crimson':
-          return 'fill="Crimson"';
+            return 'fill="Crimson"';
         case 'ForestGreen':
-          return 'fill="ForestGreen"';
+            return 'fill="ForestGreen"';
         case 'Gold':
-          return 'fill="Gold"';
+            return 'fill="Gold"';
         case 'MediumPurple':
-          return 'fill="MediumPurple"';
+            return 'fill="MediumPurple"';
         case 'SlateBlue':
-          return 'fill="SlateBlue"';
+            return 'fill="SlateBlue"';
         case 'DarkOrange':
-          return 'fill="DarkOrange"';
+            return 'fill="DarkOrange"';
         case 'White':
-          return 'fill="White"';
+            return 'fill="White"';
         case 'Black':
-          return 'fill="Black"';
+            return 'fill="Black"';
         default:
-          return '';
+            return '';
     }
 }
 
 // function to generate svg file
 function generateLogo(data) {
-    const shape = renderShapes(data.shapes);
+    const shape = renderShapes(data.shapes, data);
     const bg = renderBg(data.backgroundColor)
     const txtColor = renderText(data.textColor)
     return `
     <svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">
 
-    <${shape} ${bg} />
+    <${shape.render()} ${bg} />
 
     <text x="150" y="125" font-size="60" text-anchor="middle" ${txtColor}>${data.initials}</text>
 
